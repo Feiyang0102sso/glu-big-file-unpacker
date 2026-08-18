46b5019b1f6359763a1bc90458c6a4294d8a2aa9
Feiyang0102sso <wufeiyang2@gmail.com>
2026-08-18 13:20:54 +0800

    add 010 template

--- tests/test_hashing.py
+++
@@ -1,9 +0,0 @@
-from big_tool.analysis.hashing import cstring_to_key
-
-
-def test_cstring_to_key_is_case_sensitive_by_default():
-    assert cstring_to_key("Asset") != cstring_to_key("asset")
-
-
-def test_cstring_to_key_can_ignore_case():
-    assert cstring_to_key("Asset", ignore_case=True) == cstring_to_key("asset", ignore_case=True)
